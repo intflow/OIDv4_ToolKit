4040ca78f5a1326906dd72a7fc8f2a3fd26a6852
kmjeon <lordet01@gmail.com>
2020-05-12 09:39:07 +0900

    revised run script for multi-class

--- get_images.sh
+++ get_images.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-python3 main.py downloader --classes Human_body \
-                          --type_csv all \
-                          --Dataset /DL_data/OID2 \
+python3 main.py downloader --classes Motorcycle Bicycle \
+                          --type_csv train \
+                          --Dataset /DL_data/OID \
                           --yes \
                           --limit 5000 \
-                          --multiclasses 0 \
+                          --multiclasses 1 \
                           --n_threads 32
+
+python3 main.py downloader --classes Motorcycle Bicycle \
+                          --type_csv validation \
+                          --Dataset /DL_data/OID \
+                          --yes \
+                          --limit 200 \
+                          --multiclasses 1 \
+                          --n_threads 32
+
+python3 main.py downloader --classes Motorcycle Bicycle \
+                          --type_csv test \
+                          --Dataset /DL_data/OID \
+                          --yes \
+                          --limit 100 \
+                          --multiclasses 1 \
+                          --n_threads 32
+
 
